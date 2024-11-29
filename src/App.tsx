89fff663da16cc2599ad1452fbf30886f42ae79b
Wesam Taha <tahawesam@yahoo.com>
2024-11-29 02:25:23 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import Cookies from '@/components/pages/Cookies';
 import Pricing from '@/components/pages/Pricing';
 import CreateForm from '@/components/pages/CreateForm';
 import AIFormCreator from '@/components/pages/AIFormCreator';
+import FormBuilderProduct from '@/components/products/FormBuilder';
 
 function App() {
   return (
@@ -29,24 +30,28 @@ function App() {
       <AuthProvider>
         <Layout>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Register />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/forms" element={<Forms />} />
-            <Route path="/forms/new" element={<FormBuilder />} />
-            <Route path="/forms/create" element={<CreateForm />} />
-            <Route path="/forms/ai-create" element={<AIFormCreator />} />
-            <Route path="/forms/:formId/results" element={<FormResults />} />
-            <Route path="/templates" element={<Templates />} />
-            <Route path="/account" element={<Account />} />
-            <Route path="/help" element={<Help />} />
-            <Route path="/pricing" element={<Pricing />} />
-            <Route path="/legal/terms" element={<Terms />} />
-            <Route path="/legal/privacy" element={<Privacy />} />
-            <Route path="/legal/cookies" element={<Cookies />} />
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<Register />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/forms' element={<Forms />} />
+            <Route path='/forms/new' element={<FormBuilder />} />
+            <Route path='/forms/create' element={<CreateForm />} />
+            <Route path='/forms/ai-create' element={<AIFormCreator />} />
+            <Route path='/forms/:formId/results' element={<FormResults />} />
+            <Route path='/templates' element={<Templates />} />
+            <Route path='/account' element={<Account />} />
+            <Route path='/help' element={<Help />} />
+            <Route path='/pricing' element={<Pricing />} />
+            <Route path='/legal/terms' element={<Terms />} />
+            <Route path='/legal/privacy' element={<Privacy />} />
+            <Route path='/legal/cookies' element={<Cookies />} />
+            <Route
+              path='/products/form-builder'
+              element={<FormBuilderProduct />}
+            />
           </Routes>
         </Layout>
       </AuthProvider>
